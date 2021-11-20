a5b129640a0de3caf5c55958132e41edd3a4f390
Steve <9054646+stendur@users.noreply.github.com>
2021-11-20 09:58:54 -0500

    env support

--- app.ts
+++ app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+require('dotenv').config()
 
 const app = express();
 const port = 3000;
+const key = process.env.GOOGLE_API_KEY
 
 app.listen(port, () => {
-  console.log(`Timezones by location application is running on port ${port}.`);
+  console.log(`Timezones by location application is running on port ${key}.`);
 });
